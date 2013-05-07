c521340c0c0dbef4bb4644dd768b973ea3a0e639
krishna <ks_21285@hotmail.com>
2013-05-07 21:26:41 +0100

    cat uk coplied, user cake copied, structure created

---
+++ web server/web/include/footer.php
@@ -0,0 +1,19 @@
+
+
+
+<div id="footer" >
+    <ul id="nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="who_we_are.php">Who We Are</a></li>
+        <li><a href="what_we_do.php">What We Do</a></li>
+        <li><a href="members.php">Members</a></li>
+        <li><a href="news.php">News</a></li>
+        <li><a href="gallery.php">Photo Gallery</a></li>
+        <li><a href="contact_us.php">Contact Us</a> </li>
+    </ul>
+</div>
+
+</div> <!-- container end -->
+
+</body>
+</html>
--- web server/web/index.php
+++ web server/web/index.php
@@ -1,1 +1,24 @@
-I am inside cloud 9
+<!-- header -->
+<?php  include 'include/header.php'; ?>
+
+<!-- content -->
+<!-- bodybox is defined in krishna.css -->
+<div class="content bodybox">
+    <div class="cols ">
+
+    <!-- first column -->
+    <div class="col">
+        <h1> Welcome </h1>
+        <p>Chitwan Aid Trust UK (CAT-UK) was formed in 2004 with the aim of providing free medical support not only to the poor and destitute people but also to the people of any economic status who have accidents and come to Mahendra Hospital, Bharatpur for medical treatment .</p>
+        <p>This website has been created with the aim of generating awareness about Chitwan Aid Trust UK among all Nepalese living in different parts of the world. It is hoped that many individuals from all over the world will visit this site thus enabling us to exchange our views and ideas on varying topics to bring our links ever closer together.</p>
+        <p>We welcome any constructive comments and ideas from visitors to our website that will enable us to make this website even more interesting, vibrant, resourceful, informative and user friendly. </p>
+        <p>Happy Browsing and Best Wishes for the Future</p>
+    </div>
+
+
+
+    </div>
+</div>
+
+<!-- footer -->
+<?php include "include/footer.php"; ?>
