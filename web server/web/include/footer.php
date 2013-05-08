dc177209893217086c5391d2719127ba027db7eb
krishna <ks_21285@hotmail.com>
2013-05-08 07:31:50 +0100

    some forms added

--- web server/web/include/footer.php
+++ web server/web/include/footer.php
@@ -3,14 +3,14 @@
 
 <div id="footer" >
     <ul id="nav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="who_we_are.php">Who We Are</a></li>
-        <li><a href="what_we_do.php">What We Do</a></li>
-        <li><a href="members.php">Members</a></li>
-        <li><a href="news.php">News</a></li>
-        <li><a href="gallery.php">Photo Gallery</a></li>
-        <li><a href="contact_us.php">Contact Us</a> </li>
+        <li ><a href="index.php">Home</a></li>
+        <li ><a href="events.php">Events</a></li>
+        <li ><a href="venues.php">Venues</a></li>
+        <li ><a href="preferences.php">Preferences</a></li>
+        <li ><a href="admin.php">Sign In</a></li>
+        <li ><a href="contact_us.php">Contact Us</a> </li>
     </ul>
+
 </div>
 
 </div> <!-- container end -->
