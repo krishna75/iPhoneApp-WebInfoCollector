4b17cbfeecca8b9e0cfc1db5eea81bee333e53e3
krishna <ks_21285@hotmail.com>
2013-05-12 14:09:11 +0100

    nice home page created

---
+++ web server/web/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- saved from url=(0036)http://www.bradyoungphotography.com/ -->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<title>Cloud Nine App</title>
+
+
+<meta name="description" content="Cloud Nine App.">
+<meta name="keywords" content="app, cloud, nine, oxford, night, club, disco, music, dance, floor">
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="content-language" content="en-US">
+<meta name="copyright" content="Copyright 2013 Cloud Nine App.">
+
+<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">
+
+<style type="text/css"></style></head>
+
+<body id="home">
+
+<div id="container">
+
+<div id="logo_wrapper">
+
+<div id="logo">
+<img src="images/logo_text.png" alt="Cloud Nine App">
+</div>
+</div>
+
+<div class="clear"></div>
+
+<div id="content_side">
+
+<div id="navigation">
+
+<ul id="navlist">
+<li><a href="index.php" id="homenav">Home</a></li>
+<li><a href="events.php" id="overnav">Events</a></li>
+<li><a href="venues.php" id="landnav">Venues</a></li>
+<li><a href="genres.php" id="portnav">Preferences</a></li>
+<li><a href="contactUs.php" id="connav">Contact Us</a></li>
+<li><a href="signIn.php" id="connav">Sign In</a></li>
+</ul>
+
+</div>
+
+<div id="quote">
+
+<h5>"Lets get started in here ..."</h5>
+
+</div>
+
+</div>
+
+<div id="content_wrapper">
+
+<div id="content">
+<h1>Welcome to Cloud Nine </h1>
+<h2>Clubbing App - Get Informed </h2>
+<p>
+This app will bring the up-to-date information about the events and venues in Oxford. You will be one touch away from setting the reminder of the event you like and it will remind you just on the day. 
+</p>
+<p>
+Please, be patient, we are comming soon...
+</p>
+</div>
+
+<div class="clear"></div>
+
+<div id="content_bottom">
+
+<div class="three">
+<img src="images/logo_cloud.png" alt="events">
+<h1>Events</h1>
+<h2>Find out all the events by dates in this month in Oxford</h2>
+<br>
+<a href="event.php">find out more</a>
+</div>
+
+<div class="three">
+<img src="images/logo_cloud.png" alt="venues">
+<h1>Venues</h1>
+<h2>Find out the vents by venues with in this month in Oxford</h2>
+<br>
+<a href="venue.php">find out more</a></div>
+
+<div class="three">
+<img src="images/logo_cloud.png" alt="preferences">
+<h1>Preferences</h1>
+<h2> Find out the events by the genre of your choice</h2>
+<br><br>
+<a href="genre.php">find out more</a></div>
+</div>
+
+</div>
+
+</div>
+
+<div class="clear"></div>
+
+<div id="footer_wrapper">
+
+<div id="footer">
+<p>Cloud Nine App is a part of CNAPP Ltd. Oxford, UK. <a href="contactUs.php">Contact</a></p>
+<p>Copyright � 2013 <a href="index.php">Cloud Nine App</a>. 
+</div>
+
+</div>
+
+<script src="js/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-2565253-10";
+urchinTracker();
+</script>
+</body></html>
