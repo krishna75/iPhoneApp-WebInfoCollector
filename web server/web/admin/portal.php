a6912880afb6aa4feb703f43431f1bae38cb72f7
krishna <ks_21285@hotmail.com>
2013-05-15 05:45:13 +0100

    portal has a message div

--- web server/web/admin/portal.php
+++ web server/web/admin/portal.php
@@ -4,6 +4,12 @@ include("includes/session.php");
     echo "Dear ".$_SESSION['first_name']." ".  $_SESSION['last_name'].",<p>
     Welcome to the portal page !!! </p>";
 
+// printing message if any
+if (!empty($_GET['message'])) {
+    $message= $_GET['message'];
+    echo "<h3>".$message." </h3>";
+}
+
     $role = $_SESSION['role'];
     if ($role <=3){
         // add event
