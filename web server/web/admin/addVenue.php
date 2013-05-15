80bedb2058034ab0aa5f1376fe0a1e1889fb77c0
krishna <ks_21285@hotmail.com>
2013-05-15 05:47:50 +0100

    uploading file needs enctype=multipart-form data, and a hidden field to carry the variable

--- web server/web/admin/addVenue.php
+++ web server/web/admin/addVenue.php
@@ -1,79 +1,65 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Add A Venue</title>
-<link rel="stylesheet" type="text/css" href="css/view.css" media="all">
-<script type="text/javascript" src="js/view.js"></script>
-
-</head>
-<body id="main_body" >
-	
-	<img id="top" src="images/top.png" alt="">
-	<div id="form_container">
-	
-		<h1><a>Add A Venue</a></h1>
-		<form id="form_620316" class="appnitro" enctype="multipart/form-data" method="post" action="">
-					<div class="form_description">
-			<h2>Add A Venue</h2>
-			<p>Please provide the following information to add a venue</p>
-		</div>						
-			<ul >
+<?php
+$requiredRole = 2;
+require_once("includes/session.php");
+$title = "Add Venue";
+$action = "model_addVenue.php";
+require_once("includes/form_header.php") ;
+?>
 			
-					<li id="li_4" >
-		<label class="description" for="element_4">Company logo </label>
-		<div>
-			<input id="element_4" name="element_4" class="element file" type="file"/> 
-		</div> <p class="guidelines" id="guide_4"><small>Select a logo for the venue.
-Size of the company logo should be around 70 x 70 pixel</small></p> 
-		</li>		<li id="li_1" >
-		<label class="description" for="element_1">Venue Name </label>
-		<div>
-			<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/> 
-		</div><p class="guidelines" id="guide_1"><small>Please provide the name of the venue e.g. O2 Academy</small></p> 
-		</li>		<li id="li_2" >
-		<label class="description" for="element_2">Address </label>
-		<div>
-			<textarea id="element_2" name="element_2" class="element textarea small"></textarea> 
-		</div><p class="guidelines" id="guide_2"><small>Address of the venue</small></p> 
-		</li>		<li id="li_3" >
-		<label class="description" for="element_3">Phone </label>
-		<div>
-			<input id="element_3" name="element_3" class="element text medium" type="text" maxlength="255" value=""/> 
-		</div><p class="guidelines" id="guide_3"><small>Phone number of the venue.</small></p> 
-		</li>		<li id="li_5" >
-		<label class="description" for="element_5">Email </label>
-		<div>
-			<input id="element_5" name="element_5" class="element text medium" type="text" maxlength="255" value=""/> 
-		</div> 
-		</li>		<li id="li_6" >
-		<label class="description" for="element_6">Web Site </label>
-		<div>
-			<input id="element_6" name="element_6" class="element text medium" type="text" maxlength="255" value="http://"/> 
-		</div> 
-		</li>		<li id="li_7" >
-		<label class="description" for="element_7">Venue Photo </label>
-		<div>
-			<input id="element_7" name="element_7" class="element file" type="file"/> 
-		</div> <p class="guidelines" id="guide_7"><small>A photo that represent the venue e.g. front photo of the venue, or internal photo of the venue.</small></p> 
-		</li>		<li id="li_8" >
-		<label class="description" for="element_8">Description </label>
-		<div>
-			<textarea id="element_8" name="element_8" class="element textarea medium"></textarea> 
-		</div><p class="guidelines" id="guide_8"><small>Description of the venue includes what are the specialities  and prides of this venue</small></p> 
+		<li id="li_4" >
+            <label class="description" for="element_4">Company logo </label>
+            <div>
+                <input type="hidden" name="logoField" value="logo" />
+                <input id="logo" name="logo" class="element file" type="file"/>
+            </div>
+            <p class="guidelines" id="guide_4"><small>Select a logo for the venue. Size of the company logo should be around 70 x 70 pixel</small></p>
 		</li>
-			
-					<li class="buttons">
+        <li id="li_1" >
+            <label class="description" for="element_1">Venue Name </label>
+            <div>
+                <input id="element_1" name="name" class="element text medium" type="text" maxlength="255" value="oxford venue"/>
+            </div>
+            <p class="guidelines" id="guide_1"><small>Please provide the name of the venue e.g. O2 Academy</small></p>
+		</li>
+        <li id="li_2" >
+            <label class="description" for="element_2">Address </label>
+            <div>
+                <textarea id="element_2" name="address" class="element textarea small"></textarea>
+            </div><p class="guidelines" id="guide_2"><small>Address of the venue</small></p>
+		</li>
+        <li id="li_3" >
+            <label class="description" for="element_3">Phone </label>
+            <div>
+                <input id="element_3" name="phone" class="element text medium" type="text" maxlength="20" value=""/>
+            </div><p class="guidelines" id="guide_3"><small>Phone number of the venue.</small></p>
+		</li>
+        <li id="li_5" >
+		<label class="description" for="element_5">Email </label>
+            <div>
+                <input id="element_5" name="email" class="element text medium" type="text" maxlength="255" value=""/>
+            </div>
+		</li>
+        <li id="li_6" >
+            <label class="description" for="element_6">Web Site </label>
+            <div>
+                <input id="element_6" name="web" class="element text medium" type="text" maxlength="255" value="http://www."/>
+            </div>
+		</li>
+        <li id="li_7" >
+            <label class="description" for="element_7">Venue Photo </label>
+            <div>
+                <input type="hidden" name="photoField" value="photo" />
+                <input id="element_7" name="photo" class="element file" type="file"/>
+            </div> <p class="guidelines" id="guide_7"><small>A photo that represent the venue e.g. front photo of the venue, or internal photo of the venue.</small></p>
+		</li>
+        <li id="li_8" >
+            <label class="description" for="element_8">Description </label>
+            <div>
+                <textarea id="element_8" name="description" class="element textarea medium"></textarea>
+            </div><p class="guidelines" id="guide_8"><small>Description of the venue includes what are the specialities  and prides of this venue</small></p>
+		</li>
+		<li class="buttons">
 			    <input type="hidden" name="form_id" value="620316" />
-			    
 				<input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
 		</li>
-			</ul>
-		</form>	
-		<div id="footer">
-			 &copy Cloud Nine 2013
-		</div>
-	</div>
-	<img id="bottom" src="images/bottom.png" alt="">
-	</body>
-</html>
+<?php include("includes/form_footer.php");?>
--- web server/web/admin/includes/form_header.php
+++ web server/web/admin/includes/form_header.php
@@ -12,7 +12,7 @@
 <img id="top" src="images/top.png" alt="">
 <div id="form_container">
     <h1>&nbsp</h1>
-    <form id="form_626766" class="appnitro validate"  method="post" action="<?php echo $action; ?>">
+    <form enctype="multipart/form-data" id="form_626766" class="appnitro validate"  method="post"  action="<?php echo $action; ?>">
         <div class="form_description">
             <h2><?php echo $title; ?></h2>
             <label id="message" class="description">
