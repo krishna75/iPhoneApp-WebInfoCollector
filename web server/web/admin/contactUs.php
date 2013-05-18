c0e3cf769d7ec26fd51de28369a5b5b59d07eda1
krishna <ks_21285@hotmail.com>
2013-05-18 09:50:43 +0100

    contact us - validated

--- web server/web/admin/contactUs.php
+++ web server/web/admin/contactUs.php
@@ -7,19 +7,19 @@ include("includes/form_header.php");
 <li id="li_1">
     <label class="description" for="element_1">Full Name </label>
     <div>
-        <input id="element_1" name="name" class="element text medium" type="text" maxlength="255" value=""/>
+        <input id="name" name="name" class="required element text medium" type="text" maxlength="255" value=""/>
     </div>
 </li>
 <li id="li_2">
     <label class="description" for="element_2">Email </label>
     <div>
-        <input id="element_2" name="email" class="element text medium" type="text" maxlength="255" value=""/>
+        <input id="email" name="email" class="required email element text medium" type="text" maxlength="255" value=""/>
     </div>
 </li>
 <li id="li_3">
     <label class="description" for="element_3">Message </label>
     <div>
-        <textarea id="element_3" name="message" class="element textarea medium"></textarea>
+        <textarea id="message" name="message" class="required element textarea medium"></textarea>
     </div>
 </li>
 <li class="buttons">
