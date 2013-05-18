68c2199148b7f6b1e8e986243038ea85c1fec5a9
krishna <ks_21285@hotmail.com>
2013-05-18 11:18:10 +0100

    some codes from form-header has been pulled out as header

---
+++ web server/web/admin/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="css/view.css" media="all">
+    <link rel="stylesheet" type="text/css" href="css/krishna.css" media="all">
+    <script type="text/javascript" src="js/view.js"></script>
+    <script type="text/javascript" src="js/validatious-custom.js"></script>
+    <script type="text/javascript" src="js/calendar.js"></script>
+</head>
+<body id="main_body" >
