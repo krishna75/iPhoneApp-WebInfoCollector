817962fb468dd0f25ca27e401537a84d78a6b62c
krishna <ks_21285@hotmail.com>
2013-05-18 09:41:43 +0100

    add event - validated

--- web server/web/admin/addEvent.php
+++ web server/web/admin/addEvent.php
@@ -6,78 +6,99 @@ $title = "Add Event";
 $action = "model_addEvent.php";
 require_once("includes/form_header.php") ;
 ?>
-			
-<li id="li_9" >
-    <label class="description" for="element_9">Date * </label>
+
+    <li id="li_9">
+        <label class="description" for="element_9">Date * </label>
     <span>
-        <input id="element_9_1" name="dd" class="element text" size="2" maxlength="2" value="" type="text"> /
+        <input id="dd" name="dd" class="required element text" size="2" maxlength="2" value="" type="text"> /
         <label for="element_9_1">DD</label>
     </span>
     <span>
-        <input id="element_9_2" name="mm" class="element text" size="2" maxlength="2" value="" type="text"> /
+        <input id="mm" name="mm" class="required element text" size="2" maxlength="2" value="" type="text"> /
         <label for="element_9_2">MM</label>
     </span>
     <span>
-        <input id="element_9_3" name="yyyy" class="element text" size="4" maxlength="4" value="" type="text">
+        <input id="yyyy" name="yyyy" class="required element text" size="4" maxlength="4" value="" type="text">
         <label for="element_9_3">YYYY</label>
     </span>
 
     <span id="calendar_9">
         <img id="cal_img_9" class="datepicker" src="images/calendar.gif" alt="Pick a date.">
     </span>
-    <script type="text/javascript">
-        Calendar.setup({
-        inputField	 : "element_9_3",
-        baseField    : "element_9",
-        displayArea  : "calendar_9",
-        button		 : "cal_img_9",
-        ifFormat	 : "%B %e, %Y",
-        onSelect	 : selectEuropeDate
-        });
-    </script>
-    <p class="guidelines" id="guide_9"><small>Date of the event for example 15/04/2013</small></p>
-</li>
-<li id="li_1" >
-    <label class="description" for="element_1">Event Title* </label>
-    <div>
-        <input id="element_1" name="title" class="element text medium" type="text" maxlength="255" value=""/>
-    </div><p class="guidelines" id="guide_1"><small>Please provide the title of the event. e.g. Grand Party</small></p>
-</li>
-<li id="li_8" >
-    <label class="description" for="element_8">Description* </label>
-    <div>
-        <textarea id="element_8" name="description" class="element textarea medium"></textarea>
-    </div><p class="guidelines" id="guide_8"><small>Description of the event. Including the time, special guests, DJ, offers etc</small></p>
-</li>
-<li id="li_7" >
-    <label class="description" for="element_7">Event Photo* </label>
-    <div>
-        <input type="hidden" name="photoField" value="photo" />
-        <input id="element_7" name="photo" class="element file" type="file"/>
-    </div> <p class="guidelines" id="guide_7"><small>If you have a poster of the event please upload it here. The ideal size of the poster would be 200 x 300 pixel</small></p>
-</li>
-<li id="li_10" >
-    <label class="description" for="element_10">Genre* </label>
-    <div>
-    <select class="element select medium" id="element_10" name="genre">
-        <option value="" selected="selected"></option>
-        <?php
-            $result = mysql_query("SELECT * FROM SubGenres;") or die(mysql_error());
-            while($row = mysql_fetch_assoc($result)){
-                echo "<option value='".$row['id']."' >".$row['subgenre']."</option>";
-            }
-        ?>
-    </select>
-    </div><p class="guidelines" id="guide_10"><small>It is the genre of the event. Please select one from the drop down list.</small></p>
-</li>
+        <script type="text/javascript">
+            Calendar.setup({
+                inputField: "element_9_3",
+                baseField: "element_9",
+                displayArea: "calendar_9",
+                button: "cal_img_9",
+                ifFormat: "%B %e, %Y",
+                onSelect: selectEuropeDate
+            });
+        </script>
+        <p class="guidelines" id="guide_9">
+            <small>Date of the event for example 15/04/2013</small>
+        </p>
+    </li>
+    <li id="li_1">
+        <label class="description" for="element_1">Event Title* </label>
 
-<!-- if it is a client it is disabled -->
-    <li id="venueSelector" >
-        <label class="description" for="element_10">Venue </label>
         <div>
-            <select class="element select medium" id="element_10" name="venue">
+            <input id="title" name="title" class="required element text medium" type="text" maxlength="255" value=""/>
+        </div>
+        <p class="guidelines" id="guide_1">
+            <small>Please provide the title of the event. e.g. Grand Party</small>
+        </p>
+    </li>
+    <li id="li_8">
+        <label class="description" for="element_8">Description* </label>
+
+        <div>
+            <textarea id="description" name="description" class="required element textarea medium"></textarea>
+        </div>
+        <p class="guidelines" id="guide_8">
+            <small>Description of the event. Including the time, special guests, DJ, offers etc</small>
+        </p>
+    </li>
+    <li id="li_7">
+        <label class="description" for="element_7">Event Photo* </label>
+
+        <div>
+            <input type="hidden" name="photoField" value="photo"/>
+            <input id="photo" name="photo" class="required element file" type="file"/>
+        </div>
+        <p class="guidelines" id="guide_7">
+            <small>If you have a poster of the event please upload it here. The ideal size of the poster would be 200 x
+                300 pixel
+            </small>
+        </p>
+    </li>
+    <li id="li_10">
+        <label class="description" for="element_10">Genre* </label>
+
+        <div>
+            <select class="required element select medium" id="genre" name="genre">
+                <option value="" selected="selected"></option>
                 <?php
-                if ($_SESSION['role']== "3"){
+                $result = mysql_query("SELECT * FROM SubGenres;") or die(mysql_error());
+                while ($row = mysql_fetch_assoc($result)) {
+                    echo "<option value='" . $row['id'] . "' >" . $row['subgenre'] . "</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <p class="guidelines" id="guide_10">
+            <small>It is the genre of the event. Please select one from the drop down list.</small>
+        </p>
+    </li>
+
+    <!-- if it is a client it is disabled -->
+    <li id="venueSelector">
+        <label class="description" for="element_10">Venue </label>
+
+        <div>
+            <select class="required element select medium" id="venue" name="venue">
+                <?php
+                if ($_SESSION['role'] == "3") {
                     $result = mysql_query("SELECT v.id as id,v.name as name
                     FROM Venues as v left join ( Users_Venues as uv)
                     ON (v.id=uv.venue_id)
@@ -86,21 +107,24 @@ require_once("includes/form_header.php") ;
                     $venueId = mysql_result($result, 0, "id");
                     $venueName = mysql_result($result, 0, "name");
 
-                   echo ' <option value="'.$venueId.'" selected="selected">'.$venueName.'</option>';
+                    echo ' <option value="' . $venueId . '" selected="selected">' . $venueName . '</option>';
                 } else {
                     echo '<option value="" selected="selected"></option>';
                     $result = mysql_query("SELECT id,name FROM Venues;") or die(mysql_error());
-                    while($row = mysql_fetch_assoc($result)){
-                        echo "<option value='".$row['id']."' >".$row['name']."</option>";
+                    while ($row = mysql_fetch_assoc($result)) {
+                        echo "<option value='" . $row['id'] . "' >" . $row['name'] . "</option>";
                     }
                 }
                 ?>
             </select>
-        </div><p class="guidelines" id="guide_10"><small> Venue associated with this event</small></p>
+        </div>
+        <p class="guidelines" id="guide_10">
+            <small> Venue associated with this event</small>
+        </p>
     </li>
 
-<li class="buttons">
-    <input type="hidden" name="form_id" value="620316" />
-    <input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
-</li>
+    <li class="buttons">
+        <input type="hidden" name="form_id" value="620316"/>
+        <input id="saveForm" class="button_text" type="submit" name="submit" value="Submit"/>
+    </li>
 <?php include("includes/form_footer.php");?>
