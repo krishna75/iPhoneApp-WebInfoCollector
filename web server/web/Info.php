3eb5c639beb064d20e5d9ad0fcb33a52a15d806d
Krishna Sapkota <ks_21285@Hotmail.com>
2013-07-07 14:21:13 +0100

    WEB - typos corrected (spelling mistake)

--- web server/web/Info.php
+++ web server/web/Info.php
@@ -57,23 +57,23 @@ li>
 <div id="content">
 <h1>Info </h1>
 <p>
-Cloud nine is the latest in mobile advertising, using a unique udating system it will be frequently updated to have all the best nights around in the Oxford area.
+Cloud nine is the latest in mobile advertising, using a unique updating system it will be frequently updated to have all the best nights around in the Oxford area.
 </p>
 <p>
 The App is Free to download and works on a 30 day live calender.
-The App itself is broken down into 3 easy to use sections, Events, Venues and Prefrences.
+The App itself is broken down into 3 easy to use sections, Events, Venues and Preferences.
 </p>
 <h2>Events </h2> 
 <p>
-Events specifies the date and how many events are being hosted on that day, which are automatically updated once a Venue or Promoter uploades a new event.
+Events specifies the date and how many events are being hosted on that day, which are automatically updated once a Venue or Promoter uploads a new event.
 </p>
 <h2>Venues</h2>
 <p>
 Once selected the user can select the Venue of their choice and browse through events on screen for that specific Venue from closest date forward.
 <p/>
-<h2>Prefrences</h2>
+<h2>Preferences</h2>
 <p>
-Prefrences is the final section, it is made of up differene genres.<br />
+Preferences is the final section, it is made of up different genres.<br />
 There is a list of genres ranging from Chart music all the way to World music. <br />
 Once you have picked the type of night you want it will display all the different events specific to the genre you selected.
 </p>
@@ -86,11 +86,11 @@ Once you have picked the type of night you want it will display all the differen
 
 <div class="three">
 <h2>Membership </h2><h4>(Clients only)</h4>
-<p>There are two different types of membership .</p>
+<p>There are two different types of membership.</p>
 <h4>Venue</h4>
 <ul>
 <li><p>This membership allows you to upload to one specific Venue.</p>
-<p>Within this membership it will allow you to pick the date of an event, upload a photo of the event and add upto 4000 letters of text to discribe the event.</p>
+<p>Within this membership it will allow you to pick the date of an event, upload a photo of the event and add up to 4000 letters of text to describe the event.</p>
 </li>
 <a href="">More info</a>
 </ul> 
@@ -99,7 +99,7 @@ Once you have picked the type of night you want it will display all the differen
 <ul>
 <li><p>This membership is designed for Promoters and is slightly more expensive.</p>
 <p>The reason for this is it allows the Promoter access to upload events to more than one Venue.<br />
-Within this membership it will allow you to pick the Venue, the date of an event, upload a photo of the event and add upto 4000 letters of text to discribe the event.</p>
+Within this membership it will allow you to pick the Venue, the date of an event, upload a photo of the event and add up to 4000 letters of text to describe the event.</p>
 </li>
 <a href="">More info</a>
 </ul>
