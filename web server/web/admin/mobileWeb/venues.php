7b47fc208b93e5365744ebd1eb34fc193e2b1d31
Krishna Sapkota <ks_21285@Hotmail.com>
2013-07-07 21:44:16 +0100

    WEB - css to display image, title, subtitle and description

---
+++ web server/web/admin/mobileWeb/venues.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Venues</title>
+    <link rel="stylesheet" href="css/ks-mobile.css"/>
+</head>
+<body>
+<h1>Venues</h1>
+<?php
+$json_string =    file_get_contents("http://cnapp.co.uk/public/venuesAndEvents.php");
+$parsed_json = json_decode($json_string);
+echo $parsed_json;
+?>
+
+
+    <?php
+    foreach($parsed_json  as $venue) {
+        echo "<div class='cell'>";
+            echo "<div class='cell-image'>";
+            echo "</div>";
+            echo "<div class='cell-title'>";
+                echo $venue->name;
+            echo "</div>";
+            echo "<div class='cell-subtitle'>";
+                echo $venue->address;
+            echo "</div>";
+            echo "<div class='cell-description'>";
+                echo  "this is sample description";
+            echo "</div>";
+            echo "<div class='cell-next'>";
+                echo ">>";
+            echo "</div>";
+        echo "</div>";
+    } ?>
+
+</body>
+</html>
