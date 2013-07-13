f1b1326d1bf1d33fdd8ccf54ceca94caf5caf6bd
Krishna Sapkota <ks_21285@Hotmail.com>
2013-07-13 21:01:27 +0100

    WEB:  secure access to addVoucher, uses username and password

--- web server/web/admin/model_addVoucher.php
+++ web server/web/admin/model_addVoucher.php
@@ -5,29 +5,34 @@ require_once("includes/db_connection.php");
 
 // get the post params
 $event_id = $_GET['event_id'];
-//$event_id = null;
+$user_id = $_GET['user_id'];
+$password = $_GET['password'];
 
-if (is_null($event_id) || trim($event_id) ==""){
-    echo "invalid event id = ".$event_id;
-} else {
-    $query = " SELECT  count FROM Vouchers WHERE event_id='$event_id';";
-    $eventIdResult = mysql_query($query) or die(mysql_error());
-    $qValues = mysql_fetch_assoc($eventIdResult);
-    $count = $qValues["count"];
+if ($user_id != "cloudnineapp-voucher" || $password !="App@Cloud9" ){
+    echo "permission denied";
+}else {
+    if (is_null($event_id) || trim($event_id) ==""){
+        echo "invalid event id = ".$event_id;
+    } else {
+        $query = " SELECT  count FROM Vouchers WHERE event_id='$event_id';";
+        $eventIdResult = mysql_query($query) or die(mysql_error());
+        $qValues = mysql_fetch_assoc($eventIdResult);
+        $count = $qValues["count"];
 
-    // if event id does not  exist, create one and count one
+        // if event id does not  exist, create one and count one
 
-    if (is_null($count)){
-        echo "no event id found";
-        $query = "INSERT INTO Vouchers (event_id,count) values ('$event_id','1');";
-        $insertResult = mysql_query($query) or die(mysql_error());
-    }else
-    // if event id exists, get count, add one, and update the row
-    {
-        $count ++;
-        echo $count;
-        $query = "UPDATE Vouchers SET event_id = $event_id,count=$count WHERE event_id='$event_id';";
-        $insertResult = mysql_query($query) or die(mysql_error());
+        if (is_null($count)){
+            echo "no event id found";
+            $query = "INSERT INTO Vouchers (event_id,count) values ('$event_id','1');";
+            $insertResult = mysql_query($query) or die(mysql_error());
+        }else
+        // if event id exists, get count, add one, and update the row
+        {
+            $count ++;
+            echo $count;
+            $query = "UPDATE Vouchers SET event_id = $event_id,count=$count WHERE event_id='$event_id';";
+            $insertResult = mysql_query($query) or die(mysql_error());
+        }
     }
 }
 mysql_close($con);
