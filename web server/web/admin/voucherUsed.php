8774c04290e994ac9e5c146a3a5e88481b80e3c2
Krishna Sapkota <ks_21285@Hotmail.com>
2013-07-20 16:39:24 +0100

    WEB: display voucher count for role 3

--- web server/web/admin/voucherUsed.php
+++ web server/web/admin/voucherUsed.php
@@ -20,12 +20,48 @@ if (!empty($_GET['message'])) {
 $role = $_SESSION['role'];
 $user_id = $_SESSION['username'];
 
+
 //echo "username=".$user_id." role=".$role."<br/>";
 if ($role ==3){
 
+    $query = "SELECT
+						date,
+						DATE_FORMAT(date, '%W') as day,
+						title as event,
+						voucher,
+						count
+
+						FROM  Events
+						LEFT JOIN Venues ON Venues.id = Events.venue_id
+						LEFT JOIN Vouchers ON Vouchers.event_id = Events.id
+
+						LEFT JOIN Users_Venues as uv ON uv.venue_id = Venues.id
+						LEFT JOIN Users as u on u.id = uv.user_id
+						WHERE u.username = '$user_id'
+						GROUP BY date
+						ORDER BY date DESC
+						;";
+
+    $result = mysql_query($query) or die(mysql_error());
+    echo "<table border='1'>";
+    echo "<thead><tr>";
+    echo "<th>Date</th><th>Day</th><th>Event</th><th>Voucher</th><th>Used</th>";
+    echo "</tr></thead>";
+
+    while($row = mysql_fetch_assoc($result)){
+        echo "<tr>";
+        echo "<td>".$row['date']."</td>";
+        echo "<td>".$row['day']."</td>";
+        echo "<td>".$row['event']."</td>";
+        echo "<td>".$row['voucher']."</td>";
+        echo "<td>".$row['count']."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+
 }
 if ($role == 1){
- echo "Vouchers Used";
     $query = "SELECT
 						name as venue,
 						date,
@@ -40,7 +76,7 @@ if ($role == 1){
 						WHERE date>= '$today' AND date<='$oneMonthLater'
 
 						GROUP BY date
-						ORDER BY date ASC
+						ORDER BY date DESC
 
 						;";
 
