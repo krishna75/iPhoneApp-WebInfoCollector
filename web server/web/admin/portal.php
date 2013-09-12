69133264ef3c134a1201e092436afc24f07b1126
Krishna Sapkota <ks_21285@Hotmail.com>
2013-09-12 22:25:54 +0100

    WEB: portal has separate options for separate roles

--- web server/web/admin/portal.php
+++ web server/web/admin/portal.php
@@ -27,13 +27,7 @@ echo "<div class='admin-link'><a href='signOut.php.php'  >sign out </a></div>";
     if ($role == 0){
         $role = 999;
     }
-    if ($role <=3){
-//        $result1= mysql_query("SELECT id FROM Users WHERE username='$username';") or die(mysql_error());
-//        $row = mysql_fetch_array($result1);
-//        $num_results = mysql_num_rows($result1);
-//        if ($num_results > 0){
-//            $userId = mysql_result($result1, 0, "id");
-//        }
+    if ($role ==3){
 
         $result = mysql_query("SELECT v.id as id
                     FROM Venues as v left join ( Users_Venues as uv)
@@ -45,28 +39,34 @@ echo "<div class='admin-link'><a href='signOut.php.php'  >sign out </a></div>";
             $venueId = mysql_result($result, 0, "id");
         }
 
-        // add event
+        // add
         echo "<div class='admin-link'><a href='addEvent.php'  >add an event </a></div>";
         echo "<div class='admin-link'><a href='crud/crud_event_client.php?access=21285&venue_id=".$venueId."'>edit events </a></div>";
         echo "<div class='admin-link'><a href='voucherUsed.php' ' >see used vouchers </a></div>";
-        if ($role <=2){
-        //add venue
-            echo "<div class='admin-link'><a href='addVenue.php' >add a venue </a></div>";
-            echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Venues' >edit venues </a></div>";
-            echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Events'' >edit events </a></div>";
-        }
-        if ($role == 1){
-        //add client
-        echo "<div class='admin-link'><a href='addClient.php' >add a client </a></div>";
+    }
+    if ($role ==2){
+        //add
+        echo "<div class='admin-link'><a href='addEvent.php'  >add an event </a></div>";
+        echo "<div class='admin-link'><a href='addVenue.php' >add a venue </a></div>";
+        // edit
+        echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Venues' >edit venues </a></div>";
+        echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Events'' >edit events </a></div>";
+    }
 
-        //add promoter
-            echo "<div class='admin-link'><a href='addPromoter.php'>add a promoter </a></div>";
-        //add admin
-            echo "<div class='admin-link'><a href='addAdmin.php' >add an admin </a></div>";
-            echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Users' >edit users </a></div>";
-            echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Users_Venues' >edit users and venues </a></div>";
-            echo "<div class='admin-link'><a href='adminTasks.php'  >GoDaddy </a></div>";
-        }
+    if ($role == 1){
+        //add
+        echo "<div class='admin-link'><a href='addEvent.php'  >add an event </a></div>";
+        echo "<div class='admin-link'><a href='addVenue.php' >add a venue </a></div>";
+        echo "<div class='admin-link'><a href='addClient.php' >add a client </a></div>";
+        echo "<div class='admin-link'><a href='addPromoter.php'>add a promoter </a></div>";
+        echo "<div class='admin-link'><a href='addAdmin.php' >add an admin </a></div>";
+        // edit
+        echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Venues' >edit venues </a></div>";
+        echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Events'' >edit events </a></div>";
+        echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Users' >edit users </a></div>";
+        echo "<div class='admin-link'><a href='crud/crud.php?access=21285&table=Users_Venues' >edit users and venues </a></div>";
+        //extra
+        echo "<div class='admin-link'><a href='adminTasks.php'  >GoDaddy </a></div>";
     }
 
 
