bdb0ca854d6c235ff8f7b354622a5cdb21a86014
Krishna Sapkota <ks_21285@Hotmail.com>
2013-12-08 14:44:13 +0000

    web: returns logo in event detail

--- web server/web/public/eventDetail.php
+++ web server/web/public/eventDetail.php
@@ -4,18 +4,21 @@ include("utilities.php");
 
 $id = $_GET["event_id"];
 
-$result = mysql_query("SELECT 
-						e.id as id, 
-						e.date AS date,
-						DATE_FORMAT(date, '%W') as day,						 
-						e.title AS title, 
-						v.name AS venue,
+$result = mysql_query("SELECT
+	                    v.name AS venue,
 						v.id as venue_id,
+						v.logo as logo,
+
+						DATE_FORMAT(date, '%W') as day,
+
+						e.id as id,
+						e.date AS date,
+						e.title AS title,
 						e.description AS description,
 						e.voucher AS voucher,
 						e.voucher_photo as voucher_photo,
 						e.photo AS photo
-						
+
 						FROM Venues AS v LEFT JOIN Events AS e ON v.id = e.venue_id
 						WHERE e.id=$id;");
 
@@ -30,7 +33,8 @@ while($row = mysql_fetch_assoc($result)){
 					 "description" => $row['description'],
 					 "voucher" => $row['voucher'],
                      "voucher_photo" => "http://www.cnapp.co.uk/images/voucherPhoto/".$row['voucher_photo'],
-					 "photo" => "http://www.cnapp.co.uk/images/eventPhoto/".$row['photo']
+					 "photo" => "http://www.cnapp.co.uk/images/eventPhoto/".$row['photo'],
+                     "logo" => "http://www.cnapp.co.uk/images/logo/".$row['logo']
 					 );
   }
   echo json_encode($output);
